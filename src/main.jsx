b098e8be7b901798e3f73e40bf71604b0c9431c4
alejoherrera05-del <alejoherrera05@gmail.com>
2026-08-27 16:22:51 -0500

    Load visual QA corrections last

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
 import "./styles.css";
 import "./hero/heroScene.css";
+import "./visual-qa-fixes.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
