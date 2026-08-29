c7f8d9abb24397b522c3661391ce63f133214a18
alejoherrera05-del <alejoherrera05@gmail.com>
2026-08-29 09:38:52 -0500

    Load unified experience system and reveals

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
+import { ExperienceRevealController } from "./components/ExperienceRevealController.jsx";
 import "./styles.css";
 import "./hero/heroScene.css";
 import "./visual-qa-fixes.css";
 import "./art-direction-v3.css";
 import "./process-v4.css";
 import "./contact-v4.css";
+import "./experience-v6.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <ExperienceRevealController />
   </React.StrictMode>,
 );
