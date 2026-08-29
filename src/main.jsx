7a71b1a622af05f64a926af0a4edca0c2088be60
alejoherrera05-del <alejoherrera05@gmail.com>
2026-08-29 00:56:47 -0500

    Load contact v4 styles

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import "./hero/heroScene.css";
 import "./visual-qa-fixes.css";
 import "./art-direction-v3.css";
 import "./process-v4.css";
+import "./contact-v4.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
