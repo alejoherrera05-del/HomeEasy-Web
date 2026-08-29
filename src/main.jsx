487ec454e0d7545f84bb9fd711dc4cb3d8b68075
alejoherrera05-del <alejoherrera05@gmail.com>
2026-08-29 11:09:54 -0500

    Load HomeEasy storytelling system

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
+import { initStorytellingReveals } from "./storytellingReveal.js";
 import "./styles.css";
 import "./hero/heroScene.css";
 import "./visual-qa-fixes.css";
 import "./art-direction-v3.css";
 import "./process-v4.css";
 import "./contact-v4.css";
+import "./storytelling-v6.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
+window.requestAnimationFrame(() => initStorytellingReveals());
